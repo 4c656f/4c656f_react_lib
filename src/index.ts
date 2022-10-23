865230458386fd62718124609a5164a57b1031c3
4c656f <vlevo.yovlyo@yandex.ru>
2022-10-23 15:51:27 +0600

    fix component export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import Button from "./components/Button/Button";
 import Input from "./components/Input/Input"
+import ThemeProvider from "./components/ThemeProvider/ThemeProvider";
+import MenuItem from "./components/MenuItem/MenuItem";
+import DropDown from "./components/DropDown/DropDown";
+
 
 export {
     Button,
-    Input
+    Input,
+    ThemeProvider,
+    MenuItem,
+    DropDown
 }
